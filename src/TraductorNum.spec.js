bdc6229b80471aaa603b6686d38548690ad3495b
Adriana Tarquino <atarquinocespedes@gmail.com>
2022-06-07 11:41:31 -0400

    Se muestra los numeros menores e iguales a 10

--- src/TraductorNum.spec.js
+++ src/TraductorNum.spec.js
@@ -8,5 +8,37 @@ describe("Traducir numeros", () => {
    it("Deberia devolver las letras con solo un digito", () => {
     const repuesta = traducirNumeros(2);
     expect(repuesta).toEqual("dos");
-});
+    });
+    it("Deberia devolver las letras con solo un digito", () => {
+        const repuesta = traducirNumeros(3);
+        expect(repuesta).toEqual("tres");
+    });
+    it("Deberia devolver las letras con solo un digito", () => {
+        const repuesta = traducirNumeros(4);
+        expect(repuesta).toEqual("cuatro");
+    });
+    it("Deberia devolver las letras con solo un digito", () => {
+        const repuesta = traducirNumeros(5);
+        expect(repuesta).toEqual("cinco");
+    });
+    it("Deberia devolver las letras con solo un digito", () => {
+        const repuesta = traducirNumeros(6);
+        expect(repuesta).toEqual("seis");
+    });
+    it("Deberia devolver las letras con solo un digito", () => {
+        const repuesta = traducirNumeros(7);
+        expect(repuesta).toEqual("siete");
+    });
+    it("Deberia devolver las letras con solo un digito", () => {
+        const repuesta = traducirNumeros(8);
+        expect(repuesta).toEqual("ocho");
+    });
+    it("Deberia devolver las letras con solo un digito", () => {
+        const repuesta = traducirNumeros(9);
+        expect(repuesta).toEqual("nueve");
+    });
+    it("Deberia devolver las letras con solo un digito", () => {
+        const repuesta = traducirNumeros(10);
+        expect(repuesta).toEqual("diez");
+    });
 });
--- src/traductorNumeros.js
+++ src/traductorNumeros.js
@@ -1,8 +1,18 @@
 function traducirNumeros(numero) {
-   switch (numero) {
-       case 1: return "uno";
-       case 2: return "dos";   
-   }
+    if(numero <= 10 && numero > 0){
+        switch (numero) {
+            case 1: return "uno";
+            case 2: return "dos";
+            case 3: return "tres";
+            case 4: return "cuatro";
+            case 5: return "cinco";
+            case 6: return "seis";
+            case 7: return "siete";
+            case 8: return "ocho";
+            case 9: return "nueve";
+            case 10: return "diez";   
+        }
+    }
 }
 
 export default traducirNumeros;

5bf8b690ff8ebcd830dad7f4b45dab43a280251d
Adriana Tarquino <atarquinocespedes@gmail.com>
2022-06-07 11:25:37 -0400

    Deberia capturar un numero UI

--- src/Ui.spec.js
+++ src/Ui.spec.js
@@ -9,5 +9,12 @@ describe("Traducir un numero a palabras", () => {
             const resultado = document.querySelector("#titulo");
             expect(resultado.innerHTML).toEqual("Traducir los numeros");
         });
+        it("Deberia capturar el numero", () => {
+            const resultado = document.querySelector("#primer-numero");
+            resultado.value = "1";
+            const botton = document.querySelector("#boton-traducir");
+            botton.click();
+            //expect(resultado.innerHTML).toEqual("uno");
+        });
         
 });
--- src/presenter.js
+++ src/presenter.js
@@ -1,3 +1,8 @@
 const form = document.querySelector("#traducir-form");
 const numero = document.querySelector("#primer-numero");
+const div = document.querySelector("#resultado-div");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+});

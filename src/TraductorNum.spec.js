a9b65e781e46369bd5c574845596a08079b525e5
Adriana Tarquino <atarquinocespedes@gmail.com>
2022-06-07 11:36:41 -0400

    Devolver la palabra del digito 2

--- src/TraductorNum.spec.js
+++ src/TraductorNum.spec.js
@@ -1,14 +1,12 @@
+import traducirNumeros from './traductorNumeros.js';
 
 describe("Traducir numeros", () => {
     it("Deberia devolver las letras con solo un digito", () => {
        const repuesta = traducirNumeros(1);
        expect(repuesta).toEqual("uno");
    });
+   it("Deberia devolver las letras con solo un digito", () => {
+    const repuesta = traducirNumeros(2);
+    expect(repuesta).toEqual("dos");
 });
-
-function traducirNumeros(numero) {
-    if(numero == 1){
-        return "uno";
-    }
-    
-}
+});
---
+++ src/traductorNumeros.js
@@ -0,0 +1,8 @@
+function traducirNumeros(numero) {
+   switch (numero) {
+       case 1: return "uno";
+       case 2: return "dos";   
+   }
+}
+
+export default traducirNumeros;
